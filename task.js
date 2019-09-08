4024bc2e88336ddb1d4752b1a4ccc3abb7eb19aa
Sasha Korn <you@example.com>
2019-09-08 22:51:24 +0300

    initCommit

---
+++ task.js
@@ -0,0 +1,10 @@
+const boxen = require('boxen');
+
+console.log(boxen('I love JS', {
+  padding: 1, 
+  margin: 1, 
+  borderStyle: 'bold',
+  backgroundColor: 'blue',
+  borderColor:'magentaBright',
+  
+}));
